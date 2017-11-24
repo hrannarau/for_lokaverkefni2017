7a596c8cd362f2095ab432b2cec5fba21f239b8c
Magginetturgaur995812345 <magnusjo294@nemi.tskoli.is>
2017-11-24 12:45:09 +0000

    Add files via upload

---
+++ Ashkin.py
@@ -0,0 +1,76 @@
+#Hrannar Helgi Auðunsson og Magnús Dagur Jóhannesson
+from random import *
+import random
+print("-----------------")
+print("Welcome to Ashkin")
+print(" 1. Ninja")
+print(" 2. Viking")
+print(" 3. Wizard")
+print(" 4. Soldier")
+print(" 5. Pirate")
+print(" 6. Oger")
+print(" 7. Dragonborn")
+print(" 8. Zombie")
+print(" 9. Rock Golem")
+print("10. Clown")
+fighter1 = input("Player 1: choose your fighter: ")
+fighter2 = input("Player 2: choose your fighter: ")
+start = random.randint(1,2)
+print("Player",start,"begins")
+#Ninja
+print("Kick:       (70% 150 DMG)")
+print("Ninja Star: (50% 60 DMG 2-5 times)")
+print("Sleep:      (40% Your opponent loses two turns)")
+print("Katana:     (25% 675 DMG)")
+#Viking
+print("Axe:     (80% 60 DMG)")
+print("Life:    (60% 200+ health)")
+print("Shield:  (40% You dont take damage next 3 turns)")
+print("Berzerk: (35% You hit your axe 2-6 times)")
+#Wizard
+print("Fireball: (70% 170 DMG)")
+print("Freeze:   (50% Your opponent loses two turns)")
+print("Eldingar: (50% 120 DMG 1-4 times)")
+print("Heal:     (40% 600+ health)")
+""""""""""
+print()
+print()
+print()
+print()
+
+print()
+print()
+print()
+print()
+
+print()
+print()
+print()
+print()
+
+print()
+print()
+print()
+print()
+
+print()
+print()
+print()
+print()
+
+print()
+print()
+print()
+print()
+"""""
+
+
+
+
+
+
+
+
+
+
+
